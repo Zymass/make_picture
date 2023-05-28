b74d4b22d4a5e3d7c61a6bbcbe472ab50edfa95c
Ilya Filyaev <i.filyaev@paysenger.com>
2023-05-28 09:13:45 +0300

    Initial Commit

---
+++ MakeThePicture/MakeThePictureApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeThePictureApp.swift
+//  MakeThePicture
+//
+//  Created by Илья Филяев on 28.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct MakeThePictureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
